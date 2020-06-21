b88a6dad1d2256dfbf2b8b1d55212f99a0cb506d
Phathizwe Vilakazi <patiesoft@gmail.com>
2020-06-21 19:04:40 +0200

    store.js modified

--- src/store.js
+++ src/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    sidenav: true
+  },
+  getters: {
+    sidenav: state => state.sidenav
   },
   mutations: {
-
+    toggleSideNav: (state, value) => {
+      if (typeof value === 'boolean') {
+        state.sidenav = value;
+        return;
+      }
+      state.sidenav = !state.sidenav;
+    }
   },
   actions: {
-
+    toggleSideNav({commit}, value) {
+      commit('toggleSideNav', value)
+    }
   }
 })
