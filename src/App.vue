96598036bf73c5a2b805b36f0c150ffe9be99bc9
Machawe <machaweshongwe@gmail.com>
2020-06-25 03:57:47 +0200

    --- Auto-Git Commit ---

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,37 @@
 <!--Template-->
 <template>
   <div id="app">
-    <SideNav/>
+   
+     <mdb-navbar
+        position="top-fixed"
+        transparent
+        color="mdb"
+        dark
+        scrolling
+        class="sticky-top"
+        >
+        <mdb-navbar-brand @click.native="toggleSideNav" class="button-collapse"><mdb-icon icon="bars"/></mdb-navbar-brand>
+        <mdb-navbar-toggler>
+            <mdb-navbar-nav>
+                <mdb-nav-item href="#/" waves-fixed active>Home</mdb-nav-item>
+                <mdb-nav-item href="#/Unit1" waves-fixed active>Overview</mdb-nav-item>
+                <mdb-dropdown tag="li" class="nav-item">
+                    <mdb-dropdown-toggle slot="toggle"  waves-fixe  tag="li" outline="none" class="btn-red">UNITS</mdb-dropdown-toggle>
+                    <mdb-dropdown-menu color="red">
+                        <mdb-dropdown-item href="#introduction">Unit 1</mdb-dropdown-item>
+                        <mdb-dropdown-item href="#facts">Unit 2</mdb-dropdown-item>
+                        <mdb-dropdown-item href="#issues">Unit 3</mdb-dropdown-item>
+                        <mdb-dropdown-item href="#held">Unit 4</mdb-dropdown-item>
+                        <mdb-dropdown-item href="#unitSammary">Unit 5</mdb-dropdown-item>
+                        <mdb-dropdown-item href="#activity1">Activity 1</mdb-dropdown-item>
+                        <mdb-dropdown-item href="#activity2">Activity 2</mdb-dropdown-item>
+                        <mdb-dropdown-item href="#selfAssessment">Self-Assessment</mdb-dropdown-item>
+                    </mdb-dropdown-menu>
+                </mdb-dropdown>
+            </mdb-navbar-nav>
+        </mdb-navbar-toggler>
+    </mdb-navbar>
+
     <router-view/>
     <Footer/>
   </div>
@@ -10,11 +40,10 @@
 <script>
 //import Footer from '@/components/Footer.vue'
 import Footer from '@/components/Footer2.vue'
-import SideNav from '@/components/SideNav2.vue'
 export default {
   components:{
     Footer, 
-    SideNav
+  
   }
   
 }
